66a04712433887b83b01210dd421ad33646509ed
DAN <danigitari@gmail.com>
2023-05-31 23:06:21 +0300

    test

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,12 +1,16 @@
 import React from "react";
+import { Typewriter } from "react-simple-typewriter";
+
 import "./index.css";
 
 function Home() {
   return (
     <>
       <div className=" h-screen flex w-screen justify-between relative">
-        <div className="absolute text-white flex justify-between z-20 w-full py-5 pr-20">
-          <div className="text-2xl font-bold py-2 pl-20">James Juma</div>
+        <div className="absolute text-white flex justify-between z-20 w-full py-5 pr-20 bg-[#000007] md:bg-transparent">
+          <div className="text-lg md:text-2xl font-bold py-2 pl-5  md:pl-20 ">
+            James Juma
+          </div>
           <div className=" hidden md:flex justify-between w-1/2 right-0  list-none  text-lg">
             <li className="hover:text-lg hover:scale-[1.2] ease-in duration-200 hover:cursor-pointer py-2">
               Home
@@ -25,12 +29,36 @@ function Home() {
             </li>
           </div>
         </div>
-        <div className="absolute right-0 w-full h-full flex ">
-          <div className=" hidden md:flex bg-[#000006] w-full h-full"> </div>
-          <img src="jumah3.png" alt="" className="flex  h-full object-fit " />
+        <div
+          className=" hidden md:flex absolute right-0 h-screen bg-no-repeat bg-contain w-screen
+         "
+        >
+          <div className=" hidden md:flex w-full h-full bg-[#000006]"> </div>
+          <img src="jumah3.png" alt="" className="flex h-full right-0" />
         </div>
+        <div className="flex md:hidden bg-[url('jumah3.png')] bg-center bg-[#000006] bg-no-repeat w-full h-full"></div>
       </div>
-      <div className="absolute top-60 pl-20 text-white text-4xl ">Hello</div>
+      <div className="absolute top-60 pl-20 text-white text-4xl ">
+        Hello
+        <h1
+            className="font-bold text-white"
+        >
+          Life is simple{" "}
+          <span className="text-7xl text-bold ">
+            {/* Style will be inherited from the parent element */}
+            <Typewriter
+              words={["AWS SYSTEM ADMINISTRATOR", ]}
+              loop={5}
+              cursor
+              cursorStyle="_"
+              typeSpeed={70}
+              deleteSpeed={50}
+              delaySpeed={1000}
+         
+            />
+          </span>{" "}
+        </h1>
+      </div>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      xs: "240px",
+      sm: "480px",
+      smd: "700px",
+      md: "800px",
+      lg: "976px",
+      xl: "1440px",
+      "2xl": "1536px",
+    },
     extend: {
       backgroundImage: {
         "hero-pattern": "url('jumah1.png')",

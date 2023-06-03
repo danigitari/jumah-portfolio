9bebe81fdcc5661e1012ae8f61bd3ee1c43deecf
DAN <danigitari@gmail.com>
2023-06-03 23:05:39 +0300

    a

--- src/Home.jsx
+++ src/Home.jsx
@@ -16,24 +16,24 @@ function Home() {
       <div class="fixed  z-40 scroll-up h-12 w-12  font-bold text-xl shadow-md rounded-full bg-[#3e2cca] text-white bottom-4 right-0 hover:scale-[1.3] duration-300 mr-5 md:mr-20 flex justify-center items-center">
         <AiOutlineArrowUp />
       </div>
-      <div className="flex flex-col ">
+      <div className="flex flex-col bg-[#000006]">
         <div className="max-w-screen bg-[#000007] h-full relative">
           <div className=" h-screen flex  justify-between ">
             <div className="absolute text-white flex justify-between z-20 w-full py-5 pr-5 md:pr-20 bg-[#000007] md:bg-transparent">
               <div className="text-lg md:text-3xl font-bold py-2 pl-5  md:pl-20 ">
                 James Juma
               </div>
-              <div className=" hidden md:flex justify-between w-1/2 right-0  list-none  text-lg">
-                <li className="hover:text-lg hover:scale-[1.2] ease-in duration-200 hover:cursor-pointer  py-2">
+              <div className="hidden md:flex justify-between w-1/2 right-0  list-none  text-lg">
+                <li className="hover:text-lg  nav-title hover:scale-[1.2] ease-in duration-200 hover:cursor-pointer  py-2">
                   Home
                 </li>
-                <li className="hover:text-lg hover:scale-[1.2] ease-in duration-200 hover:cursor-pointer py-2">
+                <li className="hover:text-lg  nav-title hover:scale-[1.2] ease-in duration-200 hover:cursor-pointer py-2">
                   About
                 </li>
-                <li className="hover:text-lg hover:scale-[1.2] ease-in duration-200 hover:cursor-pointer py-2">
+                <li className="hover:text-lg  nav-title hover:scale-[1.2] ease-in duration-200 hover:cursor-pointer py-2">
                   Blog
                 </li>
-                <li className="hover:text-lg hover:scale-[1.2] ease-in duration-200 hover:cursor-pointer py-2">
+                <li className="hover:text-lg  nav-title hover:scale-[1.2] ease-in duration-200 hover:cursor-pointer py-2">
                   Mentorship
                 </li>
                 <li className="hover:text-lg hover:scale-[1.1] ease-in duration-200 hover:cursor-pointer flex items-center rounded-full bg-[#3e2cca] px-6 my-1 ">
@@ -139,22 +139,35 @@ function Home() {
           </div>
         </div>
         <div className=" pl-5 md:pl-20 bg-[#000006] pt-20 h-screen translate-y-60 md:translate-y-0">
-          <p className="text-white text-3xl  py-5 font-bold "> About Me </p>
-          <p className="text-white ">
+          <p className="text-white text-3xl  py-5 font-bold nav-title w-36 pb-10">
             {" "}
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium
-            at iure, voluptatum aperiam culpa adipisci debitis nulla modi nisi
-            sunt unde. Quia pariatur ratione fugit commodi dolores nam
-            asperiores esse omnis rem libero? Impedit placeat consectetur
-            perspiciatis quos odit veniam labore. Nesciunt deserunt voluptate
-            voluptatum, unde quisquam, doloremque pariatur accusantium ipsa
-            rerum eveniet culpa officia fugiat mollitia in deleniti nobis
-            molestias eius vero dolorum consectetur? Voluptas asperiores esse
-            aliquid ea animi quaerat necessitatibus consectetur veniam nulla
-            repellendus sint quae reprehenderit laudantium, autem qui a atque
-            veritatis minus nihil iure quas id harum. Molestias quaerat
-            praesentium consequuntur officiis facilis eligendi iusto.{" "}
+            About Me{" "}
           </p>
+          <div className="flex flex-col md:flex-row ">
+            <div className="w-full md:w-1/2 text-white pr-10 text-lg">
+              I am a highly motivated and self-driven individual with Solution
+              Architect experience in AWS Cloud. I’m good at designing and
+              deploying scalable, reliable, and fault-tolerant workloads in AWS
+              Cloud, currently working at Silicon Overdrive. I am well
+              organized, detail-oriented, and a team player with prolific
+              leadership and soft skills. I possess strong analytical, research,
+              and time management skills with a high degree of commitment to my
+              work. I am a Co-founder of Resource Cloud Academy, a community
+              made up of young cloud enthusiasts. I lead the cloud community
+              members in executing real-world projects through hands-on lab
+              activities in AWS.
+              <button className="rounded-full py-2 px-6 text-white bg-[#3e2cca] mt-20 ">
+                Download CV
+              </button>
+            </div>
+            <div className="w-full md:w-1/2">
+              <img
+                src="juma-aws-2.jpg"
+                className="w-full  px-5 h-96 object-scale-down  gg"
+                alt=""
+              />
+            </div>
+          </div>
         </div>
       </div>
     </>
